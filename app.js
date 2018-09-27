ec9010195e6966718b3870a98beb8431d33703b0
Gupta, Ronak (CORP) <Ronak.Gupta@adp.com>
2018-09-27 13:43:01 -0400

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const path = require('path');
+const usdl = require('parse-usdl');
 
-app.use(express.static('public'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+//app.use(express.bodyParser());
+
+app.use(bodyParser.json());
 
 
 app.listen(process.env.PORT||3600,()=>{
@@ -12,6 +19,22 @@ app.listen(process.env.PORT||3600,()=>{
 
 app.get('/',(req,res)=>{
 
-res.sendFile(path.join(__dirname, '../public', 'index.html'));
+res.render('index');
+
+});
+
+app.get('/ThankYou', (req,res)=>{
+
+  res.sendFile(__dirname+'/views/ThankYou.ejs');
+})
+
+app.post('/endpoint',(req,res)=>{
+let obj = req.body;
+
+var dlData = usdl.parse(obj.replace(/\r/g, '\n'),{suppressErrors: true});
+//
+ console.log(JSON.stringify(dlData, null, 2));
+
+res.send(JSON.stringify(dlData, null, 2));
 
 })
